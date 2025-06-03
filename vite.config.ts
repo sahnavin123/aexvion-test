acadf74f3ab792ec949a4bc5d61b4b71f1c36fc6
navin <95286412+sahnavin123@users.noreply.github.com>
2025-06-04 00:13:35 +0530

    updated the script

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/aexvion-test",
+  base: "/aexvion-test/",
   build: {
     outDir: "dist",
   },
